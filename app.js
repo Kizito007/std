f57c7bc7f10391925a9a5592e02ac53d86bffe72
Kizito007 <nwakakelechi49@gmail.com>
2020-07-26 14:19:46 +0100

    new change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'), app = express(), bodyParser= require('body-parser');
 //Heroku Deploy 
-port = process.env.PORT || 3001;
+port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + "/public"));
